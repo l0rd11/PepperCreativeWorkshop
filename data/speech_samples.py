cc6f80165f673426ebc47f6d4e55790cf70b4095
fifimajster <filipsondej@protonmail.com>
2019-03-09 23:01:29 +0100

    controller for pepper behaviours create setup script add old behaviours

---
+++ data/speech_samples.py
@@ -0,0 +1,26 @@
+"""
+Predefined texts that can later be said by pepper.
+Feel free to add any new texts.
+"""
+
+dobrze = """
+    Nie ma tak że dobrze, albo że niedobrze... Gdybym miał powiedzieć co cenię w życiu najbardziej, 
+    powiedziałbym, że ludzi. Ludzi, którzy podali mi pomocną dłoń, kiedy sobie nie radziłem, 
+    kiedy byłem sam i co ciekawe, to właśnie przypadkowe spotkania wpływają na nasze życie. 
+    Chodzi o to, że kiedy wyznaje się pewne wartości, nawet pozornie uniwersalne, 
+    bywa, że nie znajduje się zrozumienia, które, by tak rzec, które pomaga się nam rozwijać, 
+    ja miałem szczęście, by tak rzec, ponieważ je znalazłem. I dziękuję życiu, dziękuję mu, życie to śpiew, 
+    życie to taniec, życie to miłość... Wielu ludzi pyta mnie o to samo: "ale jak ty to robisz, 
+    skąd czerpiesz tę radość?" A ja odpowiadam, że to proste! To umiłowanie życia, to właśnie ono sprawia, 
+    że dzisiaj na przykład buduję maszyny, a jutro, kto wie, dlaczego by nie, 
+    oddam się pracy społecznej i będę ot choćby sadzić marchew...
+"""
+
+yay = [
+    'super!',
+    'wspaniale!',
+    'wybornie!',
+    'łał!',
+]
+
+error = 'pi pi pi'
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# check os
+uname_out=$(uname -s)
+case ${uname_out} in
+    Linux)      installer='sudo apt install' ;;
+    Darwin)     installer='brew install' ;;
+    *)          echo 'Your system is not supported, choose installer manually.'
+                exit 1
+esac
+
+${installer} python3.6
+${installer} python3.6-venv
+
+# set up virtualenv
+PROJECT_PATH=$(dirname $0)
+python3.6 -m venv ${PROJECT_PATH}/venv
+source ${PROJECT_PATH}/venv/bin/activate
+pip install -r ${PROJECT_PATH}/requirements.txt
+chmod u+x ${PROJECT_PATH}/controller.py
+
+echo 'Installed successfully'
+
+
+######################
+#ssh nao@192.168.1.101
+#cd mqtt
+#qicli call ALTabletService._openSettings
+#./startMqttBroker.sh &
