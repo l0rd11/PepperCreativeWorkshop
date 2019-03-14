bca33a73457bd66b205ef52043c016381defb89a
fifimajster <filipeyro@gmail.com>
2019-03-14 01:23:54 +0100

    launch and stop behaviors using naoqi better help message

--- controller.py
+++ controller.py
@@ -5,48 +5,22 @@
 import datetime
 import logging
 import random
+import sys
+import textwrap
+import time
+from collections import OrderedDict
 
 import paho.mqtt.publish as publish
+import qi
 from IPython import get_ipython
 from pyfiglet import figlet_format
 
 from data.speech_samples import *
 
+
 logger = logging.getLogger('pepperer')
 NAO_IP = '192.168.1.101'
-
-
-def exc_handler(self, etype, value, tb, tb_offset=None):
-    """
-    Replace default ipython behaviour when input cannot be parsed normally.
-
-    Instead of raising SyntaxError or NameError, first check if input can
-    be interpreted as some alias.
-    """
-    # parse
-    if etype == SyntaxError:
-        cmd, message = value.text.split(' ', 1)
-        message = message.strip()
-    else:
-        # etype == NameError
-        cmd = str(value)[6:-16]
-        # because str(value) has form: "name '...' is not defined"
-        message = None
-
-    if cmd not in aliases:
-        # no such alias defined, so use default ipython behaviour
-        return self.showtraceback()
-
-    # run
-    func = aliases[cmd]
-    if message == '?':
-        print('Alias for function "{}".'.format(func.__name__))
-        print(func.__doc__)
-    else:
-        func(message)
-
-
-get_ipython().set_custom_exc((SyntaxError, NameError), exc_handler)
+NAO_PORT = 9559
 
 
 def say(message):
@@ -137,6 +111,58 @@ def video_end(_):
                    hostname=NAO_IP)
 
 
+def get_behaviors(_):
+    """
+    Know which behaviors are on the robot.
+    """
+    # based on http://doc.aldebaran.com/2-5/naoqi/core/albehaviormanager.html
+
+    names = behavior_mng_service.getInstalledBehaviors()
+    print "Behaviors on the robot:"
+    print names
+
+    names = behavior_mng_service.getRunningBehaviors()
+    print "Running behaviors:"
+    print names
+
+
+def launch_behavior(behavior_name):
+    """
+    Launch behavior.
+
+    Example:
+        l dance
+    """
+    # Check that the behavior exists.
+    if not behavior_mng_service.isBehaviorInstalled(behavior_name):
+        print "Behavior not found."
+        return
+
+    # Check that it is not already running.
+    if behavior_mng_service.isBehaviorRunning(behavior_name):
+        print "Behavior is already running."
+        return
+
+    # Launch behavior. This is a blocking call, use _async=True if you do not
+    # want to wait for the behavior to finish.
+    behavior_mng_service.runBehavior(behavior_name, _async=True)
+    time.sleep(0.5)
+
+
+def kill_behavior(behavior_name):
+    """
+    Kill behavior.
+
+    Example:
+        k dance
+    """
+    if behavior_mng_service.isBehaviorRunning(behavior_name):
+        behavior_mng_service.stopBehavior(behavior_name)
+        time.sleep(1.0)
+    else:
+        print "Behavior is already stopped."
+
+
 def quit_(_):
     """
     Quit whole program.
@@ -145,22 +171,77 @@ def quit_(_):
 
 
 def print_help(_):
-    print('\nalias \tfunction')
+    print('''\nalias    function''')
     print('--------------------')
     for key, value in aliases.items():
-        print('{} \t{}'.format(key, value.__name__))
-    print('\nfor more info about some alias, type "<alias> ?"')
+        print('{:8} {}'.format(key, value.__name__))
+    print(textwrap.dedent('''
+    for more info about some alias, type "<alias> ?"
+    
+    to create your own alias type:
+    aliases['cmd'] = your_function
+    after that typing:
+    cmd message   <=>   your_function('message')
+    cmd           <=>   your_function(None)
+    '''))
 
 
-aliases = {
-    's': say,
-    'ss': say_saved,
-    'p': publish_to_topic,
-    'vb': video_begin,
-    've': video_end,
-    'q': quit_,
-    'h': print_help,
-}
+aliases = OrderedDict([
+    ('s', say),
+    ('ss', say_saved),
+    ('p', publish_to_topic),
+    ('vb', video_begin),
+    ('ve', video_end),
+    ('g', get_behaviors),
+    ('l', launch_behavior),
+    ('k', kill_behavior),
+    ('q', quit_),
+    ('h', print_help),
+])
+
+
+def exc_handler(self, etype, value, tb, tb_offset=None):
+    """
+    Replace default ipython behavior when input cannot be parsed normally.
+
+    Instead of raising SyntaxError or NameError, first check if input can
+    be interpreted as some alias.
+    """
+    # parse
+    if etype == SyntaxError:
+        cmd, message = value.text.split(' ', 1)
+        message = message.strip()
+    else:
+        # etype == NameError
+        cmd = str(value)[6:-16]
+        # because str(value) has form: "name '...' is not defined"
+        message = None
+
+    if cmd not in aliases:
+        # no such alias defined, so use default ipython behavior
+        return self.showtraceback()
+
+    # run
+    func = aliases[cmd]
+    if message == '?':
+        print('Alias for function "{}".'.format(func.__name__))
+        print(textwrap.dedent(func.__doc__))
+    else:
+        func(message)
+
+
+get_ipython().set_custom_exc((SyntaxError, NameError), exc_handler)
+
+
+session = qi.Session()
+try:
+    session.connect("tcp://{}:{}".format(NAO_IP, NAO_PORT))
+except RuntimeError:
+    print ("Can't connect to Naoqi at ip \"" + NAO_IP + "\" on port " + str(NAO_PORT) +".\n"
+           "Please check your script arguments. Run with -h option for help.")
+    sys.exit(1)
+behavior_mng_service = session.service("ALBehaviorManager")
+
 
 print(figlet_format('Pepperer', font='graffiti'))
 print('\nfor help type h')
